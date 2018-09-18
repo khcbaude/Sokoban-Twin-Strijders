915d346f9615038bc14b02a971c7aac8aaf356e2
Koen B <khcbaude@avans.nl>
2018-09-18 17:21:43 +0200

    Stuff

--- Sokoban/Model/Doolhof.cs
+++ Sokoban/Model/Doolhof.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 
 namespace Sokoban
@@ -25,8 +26,22 @@ namespace Sokoban
 
         public void loadLevel()
         {
-            string[] lines = System.IO.File.ReadAllLines(@"Levels\LEVEL1.txt");
-            Console.WriteLine(lines[0]);
+            //string[] lines = System.IO.File.ReadAllLines(path: @"Levels\LEVEL1.txt");
+            //Console.WriteLine(lines[0]);
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader("TextFile1.txt"))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    String line = sr.ReadToEnd();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
